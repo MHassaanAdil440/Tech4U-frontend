09dcd3648be78e3aee32bf8195957c4d4be53b24
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-10-04 14:40:17 +0500

    Working on cards

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ import Navbar from "./components/Navbar";
 import Drawer from "./components/Drawer";
 import CategorySection from "./components/CategorySection"
 import Slider from "./components/Slider";
+import Cards from "./components/Cards";
 function App() {
+  const img1="https://cdn.pixabay.com/photo/2016/03/27/19/43/samsung-1283938_1280.jpg"
+  const img2="https://cdn.pixabay.com/photo/2018/01/08/02/34/technology-3068617_1280.jpg"
+  const img3="https://cdn.pixabay.com/photo/2016/06/28/05/10/laptop-1483974_1280.jpg"
   return (
     <div className="">
       <BrowserRouter>
         
       <Navbar/>
+      <Cards title1="Andriod" img1={img1} title2="Iphone X" img2={img2} title3="Laptop" img3={img3}  />
       {/* <Signup/> */}
       {/* <Drawer/> */}
-      <CategorySection/>
-      <Slider/>
+      {/* <CategorySection/>
+      <Slider/> */}
       {/* <Login /> */}
-      <MyFooter />
+      {/* <MyFooter /> */}
       </BrowserRouter>
     </div>
   );
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// function Slider() {
-//   return (
-// <div class="carousel-container" >
-//   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="height: 100%;">
-//     <div class="carousel-inner">
-//       <div class="carousel-item active">
-//         <img src="https://cdn.pixabay.com/photo/2016/06/28/05/10/laptop-1483974_1280.jpg" class="d-block w-100 h-100" alt="Image 1"/>
-//       </div>
-//       <div class="carousel-item">
-//         <img src="https://cdn.pixabay.com/photo/2018/01/08/02/34/technology-3068617_1280.jpg" class="d-block w-100 h-100" alt="Image 2"/>
-//       </div>
-//     </div>
-//     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-//       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Previous</span>
-//     </button>
-//     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-//       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Next</span>
-//     </button>
-//   </div>
-// </div> 
-
-  
-//   );
-// }
-
-// export default Slider;
 import React from 'react';
 import { Link } from 'react-router-dom';
 
